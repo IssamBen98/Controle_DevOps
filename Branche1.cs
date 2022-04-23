16cf6d61509155241384e39158e8cbb71b392d4e
IssamBen98 <benatik.issam@gmail.com>
2022-04-23 12:01:38 +0000

    modif_branche1

---
+++ Branche1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controle_DevOps
+{
+    public class Branche1
+    {
+        private int Attribute;
+
+        public Branche1(int attribute)
+        {
+            Attribute = attribute;
+        }
+    }
+}
